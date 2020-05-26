c7976eb1c7744adc89058587f693ef956d1848ca
Vince <vincent.lagrassa@gmail.com>
2020-05-25 22:02:34 -0400

    Set up basic grid and mouse

--- src/Main.elm
+++ src/Main.elm
@@ -32,9 +32,28 @@ main =
     }
 
 type alias Model = 
-  { mouseLocation : Coordinate }
+  { mouseLocation : Coordinate
+  , mapHeight : Int
+  , mapWidth : Int
+  , shapes : List MapShape
+  }
 
 type alias Coordinate = { x:Int, y:Int }
+
+type alias MapShape =
+  { shape : Shape
+  , style : MapStyle
+  }
+
+type Shape
+  = Polygon (List GridPoint)
+
+type alias GridPoint = (Int, Int)
+
+type MapStyle
+  = Wall
+  | Ground
+
 
 type Msg
   = NullMsg
@@ -51,7 +70,11 @@ init () = (initModel, Cmd.none)
 
 initModel : Model
 initModel = 
-  { mouseLocation = { x = 0, y = 0 } }
+  { mouseLocation = { x = 0, y = 0 }
+  , mapHeight = 20
+  , mapWidth = 20
+  , shapes = []
+  }
 
 
 
@@ -63,13 +86,18 @@ subscriptions model = Sub.none
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = case msg of
-  NullMsg -> (model, Cmd.none)
-  MouseMove xpos ypos -> ( { mouseLocation = { x = xpos, y = ypos }}, Cmd.none )
+  NullMsg ->
+    (model, Cmd.none)
+  MouseMove xpos ypos ->
+    ( { model | mouseLocation = { x = xpos, y = ypos }}, Cmd.none )
 
 
 setMouseLocation : C.Point -> Msg
 setMouseLocation p =
   MouseMove (floor (Tuple.first p)) (floor (Tuple.second p))
+
+
+
 
 -- View --------------------------------------------------------------
 
@@ -81,20 +109,96 @@ view model =
             |> C.filled (C.uniform Color.lightGrey)
             |> E.onMouseMove setMouseLocation
             |> R.svg
-    circ = C.circle 10
-            |> C.filled (C.uniform Color.red)
+    map = [draw_shapes, draw_grid, draw_bg]
+            |> List.map (\f -> f model)
+            |> C.group
+            |> E.onMouseMove setMouseLocation
             |> R.svg
+    mouse = make_mouse model
   in
     Html.div []
       [ Html.div [Attr.align "center"] [ Html.text msg ]
       , Html.div [ Attr.align "center" ]
-                 [ rect
-                 , Html.div [ Attr.style "position" "absolute"
-                            , Attr.style "top" ((fromInt model.mouseLocation.y) ++ "px")
-                            , Attr.style "left" ((fromInt model.mouseLocation.x) ++ "px") ]
-                            [ circ ] ] ]
+                 [ map
+                 , mouse
+                 ]
+      ]
 
 
 
 
--- Other -------------------------------------------------------------
+-- Drawing Map Objects -----------------------------------------------
+
+draw_bg : Model -> C.Collage Msg
+draw_bg model =
+  let
+    width  = scale (model.mapWidth  + 2)
+    height = scale (model.mapHeight + 2)
+  in
+  C.rectangle width height
+    |> C.filled (C.uniform Color.lightGrey)
+    |> C.shift (((width / 2) - (scale 1)), ((height / 2) - (scale 1)))
+
+draw_grid : Model -> C.Collage Msg
+draw_grid model =
+  let
+    grid_style = C.traced (C.solid C.thin (C.uniform Color.darkGrey))
+
+    h_grid_lines = (List.range 0 model.mapHeight)
+      |> List.map (\y -> C.segment (0, scale y) (scale model.mapWidth, scale y))
+      |> List.map grid_style
+
+    v_grid_lines = (List.range 0 model.mapWidth)
+      |> List.map (\x -> C.segment (scale x, 0) (scale x, scale model.mapHeight))
+      |> List.map grid_style
+  in
+    h_grid_lines ++ v_grid_lines
+      |> C.group
+
+draw_shapes : Model -> C.Collage Msg
+draw_shapes model =
+  let
+    make_collage shape = case shape.style of
+      Wall   -> C.filled (C.uniform Color.black) <| convert_shape shape.shape
+      Ground -> C.filled (C.uniform Color.white) <| convert_shape shape.shape
+  in
+    model.shapes
+      |> List.map make_collage
+      |> C.group
+
+
+make_mouse : Model -> Html Msg
+make_mouse model =
+  let
+    pointer = C.circle 10
+      |> C.filled (C.uniform Color.red)
+      |> E.onMouseMove setMouseLocation
+      |> R.svg
+
+    top_dist =  (fromInt (model.mouseLocation.y - 10)) ++ "px"
+    left_dist = (fromInt (model.mouseLocation.x - 10)) ++ "px"
+  in
+    Html.div [ Attr.style "position" "absolute"
+             , Attr.style "top"  top_dist
+             , Attr.style "left" left_dist
+             ]
+             [ pointer ]
+
+
+
+
+-- Converting Grid to Collage ----------------------------------------
+
+scaling_factor = 35
+
+scale : Int -> Float
+scale = (*) scaling_factor << toFloat
+
+scale_gridpoint : GridPoint -> C.Point
+scale_gridpoint = Tuple.mapBoth scale scale
+
+convert_shape : Shape -> C.Shape
+convert_shape shape =
+  case shape of
+    Polygon ps -> C.polygon <| List.map scale_gridpoint ps
+
