c652eec0fdb955243235364ae7dd8330084668b9
Vince <vincent.lagrassa@gmail.com>
2020-05-27 18:37:51 -0400

    Calculate point where lines intersect

--- src/Grid.elm
+++ src/Grid.elm
@@ -83,3 +83,42 @@ union a b = Nothing
 -- If two shapes overlap, return their intersection; if not, return Nothing
 intersection : Shape -> Shape -> Maybe Shape
 intersection a b = Nothing
+
+
+
+
+line_intersect : (Point, Point) -> (Point, Point) -> Maybe Point
+line_intersect line_1 line_2 =
+  (calc_point line_1 line_2) |> Maybe.andThen (check_bounds line_1 line_2)
+
+
+-- Adapted from https://www.geeksforgeeks.org/program-for-point-of-intersection-of-two-lines/
+calc_point : (Point, Point) -> (Point, Point) -> Maybe Point
+calc_point ((p1_x, p1_y), (p2_x, p2_y)) ((q1_x, q1_y), (q2_x, q2_y)) =
+  let
+    p_a = p2_y - p1_y
+    p_b = p1_x - p2_x
+    p_c = (p_a * p1_x) + (p_b * p1_y)
+
+    q_a = q2_y - q1_y
+    q_b = q1_x - q2_x
+    q_c = (q_a * q1_x) + (q_b * q1_y)
+
+    determinant = p_a * q_b - q_a * p_b
+
+  in
+    if determinant == 0 then
+      Nothing
+    else
+      Just ( (q_b*p_c - p_b*q_c) / determinant, (p_a*q_c - q_a*p_c) / determinant)
+
+check_bounds : (Point, Point) -> (Point, Point) -> Point -> Maybe Point
+check_bounds ((p1_x, p1_y), (p2_x, p2_y)) ((q1_x, q1_y), (q2_x, q2_y)) (x, y) =
+  let
+    (p_x_min, p_x_max) = (min p1_x p2_x, max p1_x p2_x)
+    (q_x_min, q_x_max) = (min q1_x q2_x, max q1_x q2_x)
+  in
+    if (p_x_min <= x && x <= p_x_max && q_x_min <= x && x <= q_x_max) then
+      Just (x,y)
+    else
+      Nothing
