b55a8ac44ca1a16e88cfe5afefbedabe0469e3af
Vince <vincent.lagrassa@gmail.com>
2020-06-07 23:01:46 -0400

    Change to camelCase

--- src/Grid/Json.elm
+++ src/Grid/Json.elm
@@ -11,62 +11,61 @@ import Grid exposing (..)
 
 -- Points
 
-encode_point : Point -> Encode.Value
-encode_point (x,y) =
+encodePoint : Point -> Encode.Value
+encodePoint (x,y) =
   Encode.object [("x", Encode.float x), ("y", Encode.float y)]
 
-point_decoder : Decode.Decoder Point
-point_decoder =
+pointDecoder : Decode.Decoder Point
+pointDecoder =
   Decode.map2 (\x y -> (x,y))
     (Decode.field "x" Decode.float)
     (Decode.field "y" Decode.float)
 
-decode_point : Encode.Value -> Maybe Point
-decode_point = Result.toMaybe << Decode.decodeValue point_decoder
+decodePoint : Encode.Value -> Maybe Point
+decodePoint = Result.toMaybe << Decode.decodeValue pointDecoder
 
 
 
 
 -- Polygons
 
-encode_polygon : Polygon -> Encode.Value
-encode_polygon = Encode.list encode_point
+encodePolygon : Polygon -> Encode.Value
+encodePolygon = Encode.list encodePoint
 
-polygon_decoder : Decode.Decoder Polygon
-polygon_decoder = Decode.list point_decoder
+polygonDecoder : Decode.Decoder Polygon
+polygonDecoder = Decode.list pointDecoder
 
-decode_polygon : Encode.Value -> Maybe Polygon
-decode_polygon = Result.toMaybe << Decode.decodeValue polygon_decoder
+decodePolygon : Encode.Value -> Maybe Polygon
+decodePolygon = Result.toMaybe << Decode.decodeValue polygonDecoder
 
 
 
 
 -- Shapes
 
-encode_shape : Shape -> Encode.Value
-encode_shape shape = case shape of
+encodeShape : Shape -> Encode.Value
+encodeShape shape = case shape of
   Polygon poly ->
-    --encode_polygon poly
     Encode.object
-      [ ("outline", encode_polygon poly)
+      [ ("outline", encodePolygon poly)
       ]
   Composite outline holes ->
     Encode.object
-      [ ("outline", encode_polygon outline)
-      , ("holes", Encode.list encode_polygon holes)
+      [ ("outline", encodePolygon outline)
+      , ("holes", Encode.list encodePolygon holes)
       ]
 
-shape_decoder : Decode.Decoder Shape
-shape_decoder =
+shapeDecoder : Decode.Decoder Shape
+shapeDecoder =
   let
     decode_outline =
-      Decode.field "outline" polygon_decoder
+      Decode.field "outline" polygonDecoder
 
     decode_holes =
-      Decode.maybe (Decode.field "holes" (Decode.list polygon_decoder))
+      Decode.maybe (Decode.field "holes" (Decode.list polygonDecoder))
         |> Decode.map (Maybe.withDefault [])
   in
     Decode.map2 (\x y -> fromOutlineAndHoles (x,y)) decode_outline decode_holes
 
-decode_shape : Encode.Value -> Maybe Shape
-decode_shape = Result.toMaybe << Decode.decodeValue shape_decoder
+decodeShape : Encode.Value -> Maybe Shape
+decodeShape = Result.toMaybe << Decode.decodeValue shapeDecoder
